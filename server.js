7441d336e1124b58a953e79249e039286dea4f7a
Elie Aro <elie.alarou@cmeoffshore.com>
2023-11-04 14:46:22 +0200

    trial2

--- server.js
+++ server.js
@@ -39,24 +39,23 @@ const subscriptions = []; // Store subscriptions here
 app.post('/api/FallDetected', (req, res) => {
   const fallStatus = req.body.fallstatus;
 
-  if (fallStatus===1) 
+  if (fallStatus) 
   {
     const notificationPayload = JSON.stringify({
       title: 'Fall Detected',
       body: 'A fall has been detected.',
     });
-    console.log(notificationPayload);
-
-   console.log(webpush.sendNotification(subscriptions[0])) 
 
     // Send the push notification to all stored subscriptions
-    Promise.all(subscriptions.map(sub => webpush.sendNotification(
-      sub, JSON.stringify(notificationPayload) )))
-      .then(() => res.status(200).json({message: 'Newsletter sent successfully.'}))
-      .catch(err => {
-          console.error("Error sending notification, reason: ", err);
-          res.sendStatus(500);
-      })
+    Promise.all(subscriptions.map((subscription) => {
+      return webpush.sendNotification(subscription, notificationPayload)
+        .catch((error) => {
+          console.error('Error sending push notification:', error);
+        });
+    }))
+    .then(() => {
+      res.status(200).json({ success: true });
+    });
   } 
 });
 
